0945e1effedff97561eed4db3079c7201d1175b4
Sahil Pundir <149288710+pundir8372@users.noreply.github.com>
2025-02-24 00:26:32 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,1 +1,7 @@
-
+def call(String dockerHubUser, String dockerHubPass, String ImageName, String ImageTag) {
+    sh """
+        echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+        docker image tag $ImageName:$ImageTag $dockerHubUser/$ImageName:$ImageTag
+        docker push $dockerHubUser/$ImageName:$ImageTag
+    """
+}
